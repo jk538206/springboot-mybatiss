62c61255be52ccf10758d8667fd962596c7f9f1d
huiliu123 <lhuii5@icloud.com>
2018-01-30 14:57:47 +0800

    01

--- src/main/java/org/spring/springboot/controller/CityRestController.java
+++ src/main/java/org/spring/springboot/controller/CityRestController.java
@@ -20,6 +20,11 @@ public class CityRestController {
     @Autowired
     private CityService cityService;
 
+    /***
+     * @param cityName
+     * @return
+     * 
+     */
     @RequestMapping(value = "/api/city", method = RequestMethod.GET)
     public City findOneCity(@RequestParam(value = "cityName", required = true) String cityName) {
         return cityService.findCityByName(cityName);
